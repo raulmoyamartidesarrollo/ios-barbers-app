6cbeab942e77e8cded77b8c794002a5f10a6f2d9
raulmoyamartidesarrollo <raulmoyamarti.desarrollo@gmail.com>
2025-05-28 19:02:10 +0200

    prierma subida de ficehros

--- ios_app_peluquerias/ios_app_peluqueriasApp.swift
+++ ios_app_peluquerias/ios_app_peluqueriasApp.swift
@@ -1,17 +1,24 @@
-//
-//  ios_app_peluqueriasApp.swift
-//  ios_app_peluquerias
-//
-//  Created by RAUL MOYA MARTI on 28/5/25.
-//
-
 import SwiftUI
+import FirebaseCore
 
+// Paso 1: Delegado de aplicación para Firebase
+class AppDelegate: NSObject, UIApplicationDelegate {
+    func application(_ application: UIApplication,
+                     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]? = nil) -> Bool {
+        FirebaseApp.configure()
+        return true
+    }
+}
+
+// Paso 2: Tu app principal
 @main
 struct ios_app_peluqueriasApp: App {
+    // Vinculamos el delegado para Firebase
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+
     var body: some Scene {
         WindowGroup {
-            StartScreen()
+            StartScreen() // o tu pantalla inicial
         }
     }
 }
